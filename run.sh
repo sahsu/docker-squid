6c153aca42674e305cffb48e12795e3ad50dbce8
Sean Hsu <sahsu.mobi@gmail.com>
2017-10-19 15:03:23 +0000

    only get 10.0.0.0/8

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 mkdir -p mkdir -p /srv/docker/squid/cache
-export IIP=`ifconfig | grep addr | grep 10\. | awk '{print $2}' | sed -e 's;addr:;;g'`
+export IIP=`ifconfig | grep addr | grep :10\. | awk '{print $2}' | sed -e 's;addr:;;g'`
 docker-compose up -d
