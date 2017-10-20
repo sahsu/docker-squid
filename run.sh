3149aa961b0c9486dbe0701dfef20704d30717ca
Sean Hsu <sahsu.mobi@gmail.com>
2017-10-20 01:49:03 +0000

    remove inet6

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p mkdir -p /srv/docker/squid/cache
-export IIP=`ifconfig | grep addr | grep :10\. | awk '{print $2}' | sed -e 's;addr:;;g'`
+export IIP=`ifconfig | grep "inet addr" | grep :10\. | awk '{print $2}' | sed -e 's;addr:;;g'`
 /usr/local/bin/docker-compose up -d
